5eadd0999e417dab346fdc31199f51538e7608af
Ali Joshany <aa6joshany@gmail.com>
2025-10-17 23:30:51 +0330

    feat: complete about me component

---
+++ app/components/AboutMe.vue.vue
@@ -0,0 +1,29 @@
+<template>
+  <section
+    class="w-full grid md:grid-cols-2 gap-[8rem] justify-items-center items-center"
+  >
+    <div class="about__content flex flex-col gap-[1.5rem]">
+      <h2 class="text-[5.2rem] font-[600]">About Me</h2>
+      <p class="text-[1.7rem]">
+        Front-End Developer specialized in React and Vue.js, passionate about
+        building fast, responsive, and visually appealing web apps with
+        Tailwind, Material UI, and SCSS.
+      </p>
+      <div class="flex flex-col gap-[1.5rem]">
+        <ProgressBarVue labal="JavaScript" :level="60" />
+        <ProgressBarVue labal="Vue Js" :level="55" />
+        <ProgressBarVue labal="React Js" :level="50" />
+        <ProgressBarVue labal="Nuxt Js" :level="30" />
+      </div>
+    </div>
+    <div
+      class="avatar max-w-[520px] border-[3px] border-[--color-orange-500] rounded-[--rounded] order-last md:order-first md:justify-self-start"
+    >
+      <img src="~/assets/img/avatar.png" alt="avatar" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>

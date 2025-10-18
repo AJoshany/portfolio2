d5c53116ddd9844a4a412404b6c9a3b90eca3ab1
Ali Joshany <aa6joshany@gmail.com>
2025-10-18 18:17:40 +0330

    feat: add page transition

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex flex-col items-center">
+  <div class="w-full flex flex-col items-center justify-between min-h-[100vh]">
   <div class="container px-[3rem] py-8 md:py-[4rem]">
     <HeaderTopNavbar />
     <slot></slot>

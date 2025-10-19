198d8618f6f9a9d8fded6e3fed3d608a3c28d92d
Ali Joshany <aa6joshany@gmail.com>
2025-10-19 09:06:50 +0330

    feat: add image to project section

--- app/components/projects/single.vue
+++ app/components/projects/single.vue
@@ -1,28 +1,26 @@
 <template>
-<div class="flex flex-col gap-[1rem]">
-    <img :src="imgSrc" :alt="title" class="rounded-[--radius-md]">
-    <p class="text-[1.4rem] text-[--color-orange-500] pt-[1rem]">{{ category }}</p>
+  <div class="flex flex-col gap-[1rem] max-w-[50rem]">
+    <img :src="imgSrc" :alt="title" class="rounded-[--radius-md]" />
+    <p class="text-[1.4rem] text-[--color-orange-500] pt-[1rem]">
+      {{ category }}
+    </p>
     <h4 class="text-[1.9rem] font-[700]">{{ title }}</h4>
-
-</div>
+  </div>
 </template>
 
 <script setup>
-
 defineProps({
-    title:{
-        type:String,
-        required:true
-    },
-     category:{
-        type:String,
-        required:true
-    },
-     imgSrc:{
-        type:String,
-        // required:true
-    }
-
-})
-
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  imgSrc: {
+    type: String,
+    // required:true
+  },
+});
 </script>
--- app/components/projects/viewer.vue
+++ app/components/projects/viewer.vue
@@ -1,23 +1,47 @@
 <template>
-<section class="flex flex-col items-center gap-[4rem]">
+  <section class="flex flex-col items-center gap-[4rem]">
     <div class="text-center md:max-w-[70%] flex flex-col gap-[1.5rem]">
-        <h2 class="text-[5.2rem] font-[600]">My Projects</h2>
-        <p class="text-[1.7rem]">
+      <h2 class="text-[5.2rem] font-[600]">My Projects</h2>
+      <p class="text-[1.7rem]">
         These are some of the tools in my developer’s toolbox. To see everything
         I’ve been sharpening, take a look at my full resume.
       </p>
     </div>
     <div class="w-full flex gap-[3.5rem] items-center justify-between">
-        <ProjectsSingle v-for="(item,index) in projects" :key="index" :title="item.title" :category="item.category" :img-src="item.imgSrc"/>
+      <ProjectsSingle
+        v-for="(item, index) in projects"
+        :key="index"
+        :title="item.title"
+        :category="item.category"
+        :img-src="item.imgSrc"
+      />
     </div>
-    <NuxtLink to="/projects" class="text-[1.4rem] text-[--color-orange-500] self-end hover:text-[--color-orange-200] transition-all">Show more...</NuxtLink>
-</section>
+    <NuxtLink
+      to="/projects"
+      class="text-[1.4rem] text-[--color-orange-500] self-end hover:text-[--color-orange-200] transition-all"
+      >Show more...</NuxtLink
+    >
+  </section>
 </template>
 
 <script setup>
+import partBankImg from "~/assets/img/part-bank.png";
+import realEstateImg from "~/assets/img/real-estate.png";
 const projects = [
-{title: 'Personal Portfolio' , category:'Nuxt Js' , imgSrc:''},
-{title: 'Part Bank' , category:'Vue Js' , imgSrc:''},
-{title: 'Real Estate' , category:'Vue Js' , imgSrc:''}
-]
+  {
+    title: "Personal Portfolio",
+    category: "Nuxt Js",
+    imgSrc: partBankImg,
+  },
+  {
+    title: "Part Bank",
+    category: "Vue Js",
+    imgSrc: partBankImg,
+  },
+  {
+    title: "Real Estate",
+    category: "Vue Js",
+    imgSrc: realEstateImg,
+  },
+];
 </script>

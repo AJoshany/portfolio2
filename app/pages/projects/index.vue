207e2378788fba19b9da8de6acc8feb58612db07
Ali Joshany <aa6joshany@gmail.com>
2025-10-19 17:50:18 +0330

    feat: add new projects

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -2,6 +2,57 @@
   <section class="pt-[3rem] text-[1.7rem] flex flex-col gap-[5rem]">
     <h2 class="text-[5.2rem] font-[600] text-center">Projects</h2>
     <div class="flex flex-col gap-[10rem] md:gap-[0]">
+
+      <!-- Portfolio -->
+
+      <div class="project flex flex-col-reverse md:flex-row md:gap-[4rem]" id="portfolio" data-aos="fade-right">
+        <div class="content flex flex-col gap-[2rem] pt-[1rem] md:pt-[3rem]">
+          <h3 class="text-[3rem] font-[600]">Portfolio</h3>
+          <p>
+           A personal portfolio website built with Nuxt.js, showcasing my projects, skills, and experience. It features a clean, responsive design and smooth navigation, designed to highlight my work effectively.
+          </p>
+          <div class="flex flex-col gap-[1rem] pt-[3rem]">
+            <div class="info">
+              <p>- Frontend:</p>
+              <p>Nuxt, Vue 3, AOS, Vite</p>
+            </div>
+            <div class="info">
+              <p>- Styling:</p>
+              <p>CSS / SCSS / Tailwind</p>
+            </div>
+            <div class="info">
+              <p>- Tools:</p>
+              <p>npm, Git</p>
+            </div>
+            <div class="info">
+              <p>- Live Demo:</p>
+              <nuxt-link
+                to="https://joshany.ir"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+            <div class="info">
+              <p>- GitHub:</p>
+              <nuxt-link
+                to="https://github.com/AJoshany/portfolio2"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+          </div>
+        </div>
+        <img
+          src="~/assets/img/real-estate.webp"
+          alt="part-bank"
+          class="max-w-[100%] md:max-w-[50%] rounded-[--radius-lg]"
+        />
+      </div>
+
       <!-- Part Bank -->
       <div class="project flex flex-col md:flex-row md:gap-[4rem]" id="part-bank"  data-aos="fade-left">
         <img
@@ -9,7 +60,7 @@
           alt="part-bank"
           class="max-w-[100%] md:max-w-[50%] rounded-[--radius-lg]"
         />
-        <div class="content flex flex-col gap-[2rem] pt-[1rem] md:pt-[3rem]">
+        <div class="content flex flex-col gap-[2rem] pt-[1rem] md:pt-[4rem]">
           <h3 class="text-[3rem] font-[600]">Part Bank</h3>
           <p>
             Part Bank is a mini banking application that simulates some of the
@@ -149,6 +200,109 @@
           </div>
         </div>
       </div>
+
+      <!-- Booking App -->
+
+       <div class="project flex flex-col-reverse md:flex-row md:gap-[4rem]" id="booking" data-aos="fade-right">
+        <div class="content flex flex-col gap-[2rem] pt-[1rem] md:pt-[3rem]">
+          <h3 class="text-[3rem] font-[600]">Booking App</h3>
+          <p>
+            A simple appointment booking website for doctors, allowing patients to schedule and manage their appointments easily. Built with a focus on clean design and user-friendly navigation.
+          </p>
+          <div class="flex flex-col gap-[1rem] pt-[3rem]">
+            <div class="info">
+              <p>- Frontend:</p>
+              <p>Vue 3, Pinia, Vue Router, Vite</p>
+            </div>
+            <div class="info">
+              <p>- Styling:</p>
+              <p>CSS</p>
+            </div>
+            <div class="info">
+              <p>- Tools:</p>
+              <p>npm, Git</p>
+            </div>
+            <div class="info">
+              <p>- Live Demo:</p>
+              <nuxt-link
+                to="https://booking-app-joshany.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+            <div class="info">
+              <p>- GitHub:</p>
+              <nuxt-link
+                to="https://github.com/AJoshany/Booking-App"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+          </div>
+        </div>
+        <img
+          src="~/assets/img/real-estate.webp"
+          alt="part-bank"
+          class="max-w-[100%] md:max-w-[50%] rounded-[--radius-lg]"
+        />
+      </div>
+
+      <!-- Dashboard Admin -->
+
+      <div class="project flex flex-col md:flex-row md:gap-[4rem]" id="dashboard" data-aos="fade-left">
+        <img
+          src="~/assets/img/finance-dashboard.webp"
+          alt="part-bank"
+          class="max-w-[100%] md:max-w-[50%] rounded-[--radius-lg]"
+        />
+        <div class="content flex flex-col gap-[2rem] pt-[1rem] md:pt-[4rem]">
+          <h3 class="text-[3rem] font-[600]">Dashboard Admin</h3>
+          <p>
+            The Financial Dashboard Project allows users to easily record and manage their transactions. It automatically analyzes income and expenses, providing clear insights through interactive charts and summaries that help users understand their spending habits and make smarter financial decisions.
+          </p>
+          <div class="flex flex-col gap-[1rem] pt-[3rem]">
+            <div class="info">
+              <p>- Frontend:</p>
+              <p>React, React Router, Rechart, Vite</p>
+            </div>
+            <div class="info">
+              <p>- Styling:</p>
+              <p>CSS / MUI</p>
+            </div>
+            <div class="info">
+              <p>- Tools:</p>
+              <p>npm, Git</p>
+            </div>
+
+            <div class="info">
+              <p>- Live Demo:</p>
+              <nuxt-link
+                to="https://dashboard-admin-phi-rose.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+            <div class="info">
+              <p>- GitHub:</p>
+              <nuxt-link
+                to="https://github.com/AJoshany/Dashboard-Admin"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-[--color-orange-500]"
+                >view</nuxt-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   </section>
 </template>

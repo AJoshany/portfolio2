713d4eedf8051d35f2ef9a27c6c9d240bb9900d9
Ali Joshany <aa6joshany@gmail.com>
2025-10-19 18:24:35 +0330

    fix: fix bug of horisontal scroll

--- app/plugins/aos.client.js
+++ app/plugins/aos.client.js
@@ -3,12 +3,11 @@ import 'aos/dist/aos.css'
 
 export default defineNuxtPlugin(() => {
   if (process.client) {
-    const isMobile = window.innerWidth < 768
     AOS.init({
-      duration: isMobile ? 400 : 800,
+      duration: 800, 
       once: true,    
       easing: 'ease-out',
-      offset: isMobile ? 50 : 120,
+      offset: 100,  
     })
   }
 })

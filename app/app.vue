fe155fca84ab6edcb43afc0217a9f567fd51a6ff
Ali Joshany <aa6joshany@gmail.com>
2025-10-20 11:51:29 +0330

    feat: add preview meta tags

--- app/app.vue
+++ app/app.vue
@@ -8,10 +8,28 @@
 
 <script setup>
 onMounted(() => {
-  window.addEventListener('load', () => {
-    AOS.refresh()
-  })
-})
+  window.addEventListener("load", () => {
+    AOS.refresh();
+  });
+});
+
+useHead({
+  title: "Ali Joshany - Front-End Developer",
+  titleTemplate: "%s - Ali Joshany",
+  meta: [
+    { property: "og:site_name", content: "Ali Joshany Portfolio" },
+    { property: "og:title", content: "Ali Joshany - Front-End Developer" },
+    { property: "og:type", content: "website" },
+    {
+      property: "og:description",
+      content:
+        "Explore my latest web projects built with Vue, React, and Tailwind.",
+    },
+    { property: "og:image", content: "https://joshany.ir/preview.png" },
+    { property: "og:url", content: "https://joshany.ir/" },
+    { name: "twitter:card", content: "summary_large_image" },
+  ],
+});
 </script>
 
 <style lang="scss">
